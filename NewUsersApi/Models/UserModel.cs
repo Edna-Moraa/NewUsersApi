9151a64c25e1373feb8ebf13fe18c23abdf296c4
Edna-Moraa <ednermoraa65@gmail.com>
2022-10-31 22:56:02 +0300

    Add project files.

---
+++ NewUsersApi/Models/UserModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NewUsersApi.Models
+{
+    public class UserModel
+    {
+        [Required]
+        [MinLength(10)]
+        public string FirstName { get; private set; }
+    }
+}
